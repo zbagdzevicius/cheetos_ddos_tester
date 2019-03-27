9095bfcf50e3a4fd884955cfdaab4351ed3752c9
zbagdzevicius <zbagdzevicius@gmail.com>
2019-03-27 16:38:08 +0200

    A

--- run.py
+++ run.py
@@ -6,16 +6,16 @@ from random import choice
 from time import sleep
 from scrapperize import crawl_user_agents
 from scrapperize import crawl_proxies
-import json
 
-proxies_path = "proxies.csv"
-with open(proxies_path) as proxies_file:
-    proxies_file_data = json.loads(proxies_file)
-user_agents = "user_agents.csv"
-with open(proxies_path) as user_agents_file:
-    user_agents_data = json.loads(user_agents_file)
+user_agents_file = "user_agents.csv"
+user_agents = csv_to_json(user_agents_file)
 
-print(proxies_file_data)
+proxies_path_file = "proxies.csv"
+proxies = csv_to_json(proxies_path_file)
+proxies = [proxy['proxy'] for proxy in proxies]
+proxy_validator = ProxyValidator(proxies)
+proxies = proxy_validator.validated_proxies
+
 
 def run(job_fn, *argv):
     arguments = []
@@ -25,36 +25,25 @@ def run(job_fn, *argv):
     job_thread.start()
 
 def send_request(headers,data,proxy):
-    response = post("https://cheetos.lt/upvote.php", headers=headers, data=data, proxies=proxy)
+    response = post("https://vote.cheetos.ee/upvote.php", headers=headers, data=data, proxies=proxy)
     print(response)
 
 def send_vote(referer, imgId, proxies):
-    proxy_type = proxies[0].keys()
-    is_socks_proxy = False
-    if proxy_type == "socks5":
-        is_socks_proxy = True
-    
     data = {
         "imgId": imgId
     }
-
     for proxy in proxies:
         user_agent = choice(user_agents)['user_agent']
         headers = {
             "accept": "*/*",
             "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "origin": "https://www.cheetos.lt",
+            "origin": "https://cheetos.lt",
             "referer": referer,
             "User-Agent": user_agent,
         }
-        if is_socks_proxy:
-            proxy = {
-                "https" : f"socks5://{proxy['socks5']}",
-            }
-        else:
-            proxy = {
-                "https" : proxy['http'],
-            }
+        proxy = {
+            "https" : proxy,
+        }
         run(send_request, headers, data, proxy)
         sleep(3)
 
@@ -62,6 +51,6 @@ def send_vote(referer, imgId, proxies):
 
 
 
-send_vote('https://www.cheetos.lt/index/galerii/images/librariesprovider3/osalejad/ranka?itemIndex=21','Ranka',new_proxies)
+send_vote('https://cheetos.lt/index/galerii/images/librariesprovider3/osalejad/ro%C5%BEyt%C4%97?itemIndex=7','Rožytė 2',proxies)
 # send_vote('https://www.cheetos.lt/index/galerii/images/librariesprovider3/osalejad/ranka?itemIndex=21','Ranka',socks_proxies)
 # send_vote('https://www.cheetos.lt/index/galerii/images/librariesprovider3/osalejad/ranka?itemIndex=21','Ranka',ssl_proxies)
